3652b9850651425f9953d4ce51aaa50ce0a56ab2
jhanviirana <156836995+jhanviirana@users.noreply.github.com>
2024-06-26 22:28:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,4 +23,23 @@ async function chechWeather(city){
     // description.innerHTML = `${weather_data.weather[0].description}`;
     humidity.innerHTML = `${weather_data.main.humidity}%`;
     wind_speed.innerHTML = `${weather_data.wind.speed}Km/h`
+
+    switch(weather_data.weather[0].main){
+        case 'Clouds':
+        weather_img.src="/assets/cloud.png";
+        break;
+        case 'Clear':
+        weather_img.src="/assets/clear.png";
+        break;
+        case 'Rain':
+        weather_img.src="/assets/rain.png";
+        break;
+        case 'Mist':
+        weather_img.src="/assets/mist.png";
+        break;
+        case 'Snow':
+        weather_img.src="/assets/snow.png";
+        break;
+    }
 }
+
